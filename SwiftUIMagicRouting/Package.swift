cba35f8be2c950a41fcac95f162d0633415dd2ce
Michel Mazumder <michel.mazumder@synesthesia.it>
2021-09-09 09:34:08 +0200

    base workspace structure

--- SwiftUIMagicRouting/Package.swift
+++ SwiftUIMagicRouting/Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIMagicRouting",
 	platforms: [
-		.macOS(.v10_15), .iOS(.v14)
+		.macOS(.v10_15),
+		.iOS(.v14)
 	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
---
+++ TestMagicRouting/Shared/TestMagicRoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMagicRoutingApp.swift
+//  Shared
+//
+//  Created by Michel on 09/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TestMagicRoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
